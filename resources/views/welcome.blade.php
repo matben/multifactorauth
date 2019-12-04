d9d5c264f41caba1c36dce9e77010cdd103883c1
Marko Bencic <mark.bencic@gmail.com>
2019-12-04 15:36:51 +0100

    welcome stranica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,41 +2,56 @@
 
 @section('content')
 
-    <div class="card">
+    {{--<div class="card">--}}
 
 
-        <div class="card-body">
-
-            <div class="alert alert-info" role="alert" align="center">
-                <h3>Vrste dostupnih autentikacija za drugi stupanj</h3>
-            </div>
-
-            <ul class="list-group">
-                <li class="list-group-item">Google Authenticator</li>
-                <li class="list-group-item">yubico yubikey</li>
-                <li class="list-group-item">AAI@EduHr SMS</li>
-                <li class="list-group-item">AAI@EduHr SimpleSQL</li>
-            </ul>
-
+        <div class="jumbotron">
+            {{--<h1 class="display-4">Hello, world!</h1>--}}
+            <p class="lead" align="center">Aplikacija za registraciju korisnika koji koriste drugi stupanj autentikacije u sustavu AAI.</p>
+            {{--<hr class="my-4">--}}
+            {{--<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>--}}
+            {{--<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>--}}
         </div>
 
-        <div class="card-body">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
-            <div class="alert alert-info" role="alert" align="center">
-                <h3>Usluge koje koriste višestupanjsku autentikaciju</h3>
-            </div>
+        {{--<div class="card-body">--}}
 
-            <ul class="list-group">
-                <li class="list-group-item">web usluga 1</li>
-                <li class="list-group-item">web usluga 2</li>
-                <li class="list-group-item">web usluga 3</li>
-                <li class="list-group-item">web usluga 4</li>
-                <li class="list-group-item">web usluga 5</li>
-                <li class="list-group-item">web usluga 6</li>
-                <li class="list-group-item">web usluga 7</li>
-            </ul>
+            {{--<div class="alert alert-info" role="alert" align="center">--}}
+                {{--<h3>Vrste dostupnih autentikacija za drugi stupanj</h3>--}}
+            {{--</div>--}}
 
-        </div>
+            {{--<ul class="list-group">--}}
+                {{--<li class="list-group-item">Google Authenticator</li>--}}
+                {{--<li class="list-group-item">yubico yubikey</li>--}}
+                {{--<li class="list-group-item">AAI@EduHr SMS</li>--}}
+                {{--<li class="list-group-item">AAI@EduHr SimpleSQL</li>--}}
+            {{--</ul>--}}
 
-    </div>
+        {{--</div>--}}
+
+        {{--<div class="card-body">--}}
+
+            {{--<div class="alert alert-info" role="alert" align="center">--}}
+                {{--<h3>Usluge koje koriste višestupanjsku autentikaciju</h3>--}}
+            {{--</div>--}}
+
+            {{--<ul class="list-group">--}}
+                {{--<li class="list-group-item">web usluga 1</li>--}}
+                {{--<li class="list-group-item">web usluga 2</li>--}}
+                {{--<li class="list-group-item">web usluga 3</li>--}}
+                {{--<li class="list-group-item">web usluga 4</li>--}}
+                {{--<li class="list-group-item">web usluga 5</li>--}}
+                {{--<li class="list-group-item">web usluga 6</li>--}}
+                {{--<li class="list-group-item">web usluga 7</li>--}}
+            {{--</ul>--}}
+
+        {{--</div>--}}
+
+    {{--</div>--}}
 @endsection
